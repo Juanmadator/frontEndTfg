171dd3831aee9a9987c959fc782f5ed90ee74f10
Juanma <juanmadametor@gmail.com>
2024-04-17 14:16:03 +0200

    Cambios al frontEND

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  public menuItems =
+    document.querySelectorAll(".menu-item");
+
+  @ViewChild('messages') messages!: ElementRef<HTMLDivElement>;
+
+  @ViewChild('messageSearch') messageSearch?: ElementRef<HTMLInputElement>;
+
+
+  ngOnInit(): void {
+    // Escucha el evento de entrada en el campo de búsqueda de mensajes
+    this.messageSearch?.nativeElement.addEventListener('keyup', this.filterMessages);
+  }
+
+
+  public changeActiveItem(item: any): void {
+    const menuItems = document.querySelectorAll(".menu-item");
+    menuItems.forEach(menuItem => {
+      menuItem.classList.remove('active');
+    });
+
+    let menuItem = item.classList.contains('menu-item') ? item : item.closest('.menu-item');
+    if (menuItem) {
+      menuItem.classList.add('active');
+    }
+
+  }
+
+  filterMessages = () => {
+    const searchTerm = (document.getElementById("message-search") as HTMLInputElement).value.toLowerCase();
+    const mensajes = document.querySelectorAll(".message");
+
+    mensajes.forEach((mensaje: any) => {
+      const h5 = mensaje.querySelector("h5");
+      if (h5) {
+        const nombre = h5.textContent?.toLowerCase();
+        const messageBody = mensaje.querySelector(".message-body");
+        if (nombre && messageBody) {
+          if (nombre.includes(searchTerm)) {
+            mensaje.style.display = "block";
+          } else {
+            mensaje.style.display = "none";
+          }
+        }
+      }
+    });
+  };
+
+
 
 }
---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,63 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+
+  public menuItems =
+  document.querySelectorAll(".menu-item");
+
+@ViewChild('messages') messages!: ElementRef<HTMLDivElement>;
+
+@ViewChild('messageSearch') messageSearch?: ElementRef<HTMLInputElement>;
+
+
+ngOnInit(): void {
+  // Escucha el evento de entrada en el campo de búsqueda de mensajes
+  this.messageSearch?.nativeElement.addEventListener('keyup', this.filterMessages);
+}
+
+
+public changeActiveItem(item: any): void {
+  const menuItems = document.querySelectorAll(".menu-item");
+  menuItems.forEach(menuItem => {
+    menuItem.classList.remove('active');
+  });
+
+  let menuItem = item.classList.contains('menu-item') ? item : item.closest('.menu-item');
+  if (menuItem) {
+    menuItem.classList.add('active');
+  }
+
+}
+
+filterMessages = () => {
+  const searchTerm = (document.getElementById("message-search") as HTMLInputElement).value.toLowerCase();
+  const mensajes = document.querySelectorAll(".message");
+
+  mensajes.forEach((mensaje: any) => {
+    const h5 = mensaje.querySelector("h5");
+    if (h5) {
+      const nombre = h5.textContent?.toLowerCase();
+      const messageBody = mensaje.querySelector(".message-body");
+      if (nombre && messageBody) {
+        if (nombre.includes(searchTerm)) {
+          mensaje.style.display = "block";
+        } else {
+          mensaje.style.display = "none";
+        }
+      }
+    }
+  });
+};
+
+
+mostrarConfig=()=>{
+  
+}
+}
